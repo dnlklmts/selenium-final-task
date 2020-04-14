4a721af3534ec9956c0595f599a86373c4ddf5e0
dnlklmts <kolomeetz.dany@yandex.ru>
2020-04-14 17:25:48 +0300

    Remove unused import and modify test-case based on product page class structure

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,4 @@
 import pytest
-from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 
 
@@ -20,4 +19,8 @@ from .pages.product_page import ProductPage
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
+    page.should_be_product_page()
     page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.check_success_message()
+    page.check_basket_info_message()
