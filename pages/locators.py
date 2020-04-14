0d1f295c489b797aaeacbe2b4244cb73a4c07ef2
dnlklmts <kolomeetz.dany@yandex.ru>
2020-04-14 14:43:10 +0300

    Add product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators():
 class LoginPageLocators():
     REG_FORM = (By.ID, 'register_form')
     LOGIN_FORM = (By.ID, 'login_form')
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CLASS_NAME, 'btn-add-to-basket')
+    PRODUCT_TITLE = (By.TAG_NAME, 'h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    SUCCESS_MSG = (By.CLASS_NAME, 'alert-success')
+    TITLE_IN_MSG = (By.CSS_SELECTOR, '.alert-success strong')
+    BASKET_TOTAL_MSG = (By.CLASS_NAME, 'alert-info')
+    TOTAL_COST = (By.CSS_SELECTOR, '.alert-info strong')
