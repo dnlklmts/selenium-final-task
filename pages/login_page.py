df53b2c85599603791bfbac8033e8731f1542080
dnlklmts <kolomeetz.dany@yandex.ru>
2020-04-16 13:04:05 +0300

    Add a new user registration method

--- pages/login_page.py
+++ pages/login_page.py
@@ -19,3 +19,9 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REG_FORM), (
                 'Registration form is not presented')
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REG_FORM_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REG_FORM_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REG_FORM_RE_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REG_BUTTON).click()
